43e37ecd828e694505e843b3f7a7b93d6180a4ca
tom_glb <goodMorning@adfad.com>
2023-04-30 22:59:23 +0800

    基本骨架å¤已å¤搭好â

--- backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -3,19 +3,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk")
 public class BotinfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("sword");
-        list.add("tiger");
-        list.add("apple");
-        return list;
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>hello world</div>
-  <router-view/>
+  <div>
+    <div>Bot昵称：{{ bot_name }}</div>
+    <div>Bot战力：{{ bot_rating }}</div>
+  </div>
+  <router-view></router-view>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default{
+  name:"APP",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    console.log("1");
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/pk/getbotinfo/",
+      type:"get",
+      success: resp => {
+          console.log("1");
+          bot_name.value = resp.name;
+          bot_rating.value = resp.rating;
+      }
+    });
+    console.log("2");
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+
+</script>
+
 <style>
+body {
+  background-size: cover;
+  background-image: url("@/assets/1_59b8f162fd-background.png");
+}
 </style>
